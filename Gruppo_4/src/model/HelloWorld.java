94d940a076b14f4331cae6294cb5a01a7e787605
riccardo-maldera <99264964+riccardo-maldera@users.noreply.github.com>
2022-03-10 11:24:31 +0100

    Update HelloWorld.java

--- Gruppo_4/src/model/HelloWorld.java
+++ Gruppo_4/src/model/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Ciao sono Lorenzo Avondo");
 		System.out.println("Ciao sono Sara Morritti");
+		System.out.println("Ciao sono Riccardo Maldera");
 		}
 
 }
